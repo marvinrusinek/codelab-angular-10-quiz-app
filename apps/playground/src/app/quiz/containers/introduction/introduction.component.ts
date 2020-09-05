9f1427f644b1ef85a1c2cf8ff9a044deaaa82999
Marvin Rusinek <marvinrusinek@gmail.com>
2020-09-05 11:06:19 -0400

    upload quiz app 09-05-20

--- apps/playground/src/app/quiz/containers/introduction/introduction.component.ts
+++ apps/playground/src/app/quiz/containers/introduction/introduction.component.ts
@@ -1,16 +1,45 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { QUIZ_DATA } from '@codelab-quiz/shared/quiz-data';
+import { Quiz } from '@codelab-quiz/shared/models/Quiz.model.ts';
+import { QuizService } from '@codelab-quiz/shared/services/quiz.service';
+
 
 @Component({
   selector: 'codelab-quiz-intro',
   templateUrl: './introduction.component.html',
-  styleUrls: ['./introduction.component.scss']
+  styleUrls: ['./introduction.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class IntroductionComponent {
+export class IntroductionComponent implements OnInit, OnDestroy {
+  quizData: Quiz[] = JSON.parse(JSON.stringify(QUIZ_DATA));
+  quizName = '';
+  unsubscribe$ = new Subject<void>();
+  imagePath = '../../../assets/images/';
 
-  constructor(private router: Router) {}
+  constructor(private quizService: QuizService,
+              private activatedRoute: ActivatedRoute
+  ) { }
 
-  startQuiz() {
-    this.router.navigateByUrl('/quiz/question/1');
+  ngOnInit(): void {
+    this.activatedRoute.url
+      .pipe(takeUntil(this.unsubscribe$))
+        .subscribe(segments => {
+          this.quizName = segments[1].toString();
+        });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  onChange($event): void {
+    if ($event.checked === true) {
+      this.quizService.setChecked($event.checked);
+    }
   }
 }
