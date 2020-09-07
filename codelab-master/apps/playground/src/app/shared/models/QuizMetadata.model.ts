901ebc12a18f92f155822e29fb09855806b528d6
Marvin Rusinek <marvinrusinek@gmail.com>
2020-09-07 15:44:24 -0400

    upload quiz files

--- codelab-master/apps/playground/src/app/shared/models/QuizMetadata.model.ts
+++
@@ -1,9 +0,0 @@
-import { Observable } from 'rxjs';
-
-export interface QuizMetadata {
-  totalQuestions: number;
-  totalQuestionsAttempted: number;
-  correctAnswersCount$: Observable<number>;
-  percentage: number;
-  completionTime: number;
-}
--- codelab-master/apps/playground/src/app/shared/models/QuizQuestion.model.ts
+++
@@ -1,8 +0,0 @@
-import { Option } from '@codelab-quiz/shared/models/Option.model';
-
-export interface QuizQuestion {
-  questionText: string;
-  options: Option[];
-  explanation: string;
-}
-type Questions = QuizQuestion[];
